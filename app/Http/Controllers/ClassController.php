4fc3c8d5335fb1593a887cc871696138ba7529c7
HP <ibrahim.keddah@gmail.com>
2024-06-10 02:16:50 +0300

    ..

--- app/Http/Controllers/ClassController.php
+++ app/Http/Controllers/ClassController.php
@@ -5,8 +5,8 @@ namespace App\Http\Controllers;
 use App\Models\classs;
 use App\Models\User;
 use App\Models\student;
-use Illuminate\support\Facades\Auth;
-use Illuminate\support\Facades\DB;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 
